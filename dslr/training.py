1feae81376dac675db36491b96537832d6b4de0c
jmouaike <john.mouaikel@gmail.com>
2023-09-11 13:12:40 +0200

    not

---
+++ dslr/training.py
@@ -0,0 +1,10 @@
+"""training.py:
+
+Programm to train a linear regression model
+based on gradient descent algorithm.
+
+a car price to mileage datasetdata.csv is required
+
+"""
+
+__author__ = "jmouaike"
