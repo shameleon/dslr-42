15b0737eadbb2c7ec9009213c9cf3d6f3a8a58b0
jmouaike <john.mouaikel@gmail.com>
2023-09-14 11:16:30 +0200

    cannot fix df index to csv

--- explore/heatmap_weights.py
+++ explore/heatmap_weights.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import sys
 
 """
@@ -10,7 +11,14 @@ https://foundations.projectpythia.org/core/matplotlib/annotations-colorbars-layo
 def main():
     file = f'logistic_reg_model/gradient_descent_weights.csv'
     df_weights = pd.read_csv(file)
-    print("+", df_weights.head())
-    
+    df_weights.set_index('Unnamed: 0', inplace=True)
+    print(df_weights.head(11))
+    houses = df_weights.columns[1:].to_list()
+    features = df_weights['Unnamed: 0'].to_list()
+    # print(df_weights[:,1:])
+    # sns.heatmap(df_weights.columns[1:].to_numpy())
+    # plt.show()
+
+
 if __name__ == "__main__":
     main()
