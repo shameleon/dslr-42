fb135ef4bb4cd203c69bc3c86b05d6833210e83b
jmouaike <john.mouaikel@gmail.com>
2023-09-26 13:11:19 +0200

    rm pynb md

--- explore/pairplot.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-
-df = pd.read_csv(f'../datasets/dataset_train.csv')
-df.head()
-# sns.pairplot(df[13:], hue="Hogwarts House", diag_kind="hist")
